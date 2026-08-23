92bf5f765bb5c17debe34769f9eb7f7449ccf6df
laolu <femiogun9590@gmail.com>
2026-08-23 16:57:00 -0400

    Add color filter

--- composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/ModalBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/ModalBottomSheet.kt
@@ -1387,14 +1387,16 @@ fun QueueItemBottomSheet(
                                     QueueItemAction.PLAYNEXT -> {
                                         Image(
                                             imageVector = SimpIcons.PlayCircle,
-                                            contentDescription = "Play Next"
+                                            contentDescription = "Play Next",
+                                            colorFilter = ColorFilter.tint(rememberSurfaceDarkColors().content),
                                         )
                                     }
 
                                     QueueItemAction.ADDTOQUEUE -> {
                                         Image(
                                             imageVector = SimpIcons.QueueMusic,
-                                            contentDescription = "Add to Queue"
+                                            contentDescription = "Add to Queue",
+                                            colorFilter = ColorFilter.tint(rememberSurfaceDarkColors().content),
                                         )
                                     }
 
